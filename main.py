8f50b3afd3d2d59251133c30c6e702832e8344a5
Jeremy Ward <jeremy@jmichaelward.com>
2020-03-01 10:21:48 -0600

    Update main.py to import print_content from lib and run its main method.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import the print_content module from the lib directory.
+from lib import print_content
+
+# Note: This is made possible by the __init__.py file in the lib directory, which Python uses to identify
+# a directory as a collection of modules. Using this approach, I should be able to split up my code into
+# separate bits of related functionality, much like I would already do with class-based hierarchies in PHP.
+
+
+def main():
+    print_content.main()
+
+
+main()
